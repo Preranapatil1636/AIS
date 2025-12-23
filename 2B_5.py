290e882ac80251f0e3d38550c3378c4057716df3
anuradhamohite-cloud <anuradha.mohite@walchandsangli.ac.in>
2025-12-23 16:10:58 +0530

    Create 2B_5.py

---
+++ 2B_5.py
@@ -0,0 +1,5 @@
+name = ["Ananya" , "Prerana" , "Sanika" , "Aditi" , "Jasmin"]
+age = [20 , 21 , 22 , 23 , 24]
+
+for i in range(len(name)):
+    print("My name is" , name[i] ," , my age is" , age[i])
